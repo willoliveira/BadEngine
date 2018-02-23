235b85a74a2f18e0736f4935ccd471655a9dc884
Willian Gomes de Oliveira <willian.goliveira@outlook.com>
2018-02-22 23:15:35 -0300

    Colocando o player e a colisão de uma maneira bem rebuscada ainda.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
 	entry: './src/index.ts',
-	devtool: 'inline-source-map',
+	devtool: 'cheap-eval-source-map',
+	devServer: {
+		contentBase: path.join(__dirname, "dist"),
+		port: 9000,
+		hot: true
+	},
 	module: {
 		rules: [
 			{
@@ -18,5 +24,8 @@ module.exports = {
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist')
-	}
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin()
+	]
 };
